776ba69051ea32c2af589aab9f262f2ddbb918c1
risamatsumoto1104 <risashibata114@gmail.com>
2024-10-05 22:10:09 +0900

    Todoアプリ初級編（完了）

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TodoController::class, 'index'] );
+Route::post('/todos', [TodoController::class, 'store'] );
+Route::patch('/todos/update', [TodoController::class, 'update'] );
+Route::delete('/todos/delete', [TodoController::class, 'destroy'] );
